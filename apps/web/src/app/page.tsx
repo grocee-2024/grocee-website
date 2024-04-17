0212a1d47ac4b6d88c3cc442b2c7dd6b54444aed
Oleksii <oleksii.lavka.dev@gmail.com>
2024-04-17 12:27:36 +0300

    feat: implemented date and time input

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,79 +1,17 @@
 'use client'
 
-import { Input, NewsCard, ProductCard } from 'ui'
-import { action } from './actions'
-import { CustomSelect } from './CustomSelect'
-import { motion } from 'framer-motion'
+import { Input } from 'ui'
 
 export default function Home() {
-  const container = {
-    hidden: { opacity: 1, scale: 0 },
-    visible: {
-      opacity: 1,
-      scale: 1,
-      transition: {
-        delayChildren: 0.2,
-        staggerChildren: 0.2,
-      },
-    },
-  }
-
-  const item = {
-    hidden: { y: 20, opacity: 0 },
-    visible: {
-      y: 0,
-      opacity: 1,
-    },
-  }
-
-  // return (
-  //   <Input
-  //     type='time'
-  //     ariaLabel='time-picker'
-  //     trailingComplex={{ start: 'Clock' }}
-  //     label='Choose a time'
-  //   />
-  // )
-
   return (
-    <motion.div
-      className='grid-desktop m-[100px]'
-      variants={container}
-      initial='hidden'
-      animate='visible'
-    >
-      <NewsCard
-        link='/'
-        title='News'
-        tag='Tag'
-        titleColor='black'
-        animationProps={{ variants: item }}
-        className='col-span-3'
+    <>
+      <Input
+        type='text'
+        ariaLabel='time-picker'
+        trailingComplex={{ start: 'Clock' }}
+        label='Choose a time'
+        placeholder='00-00'
       />
-      <NewsCard
-        link='/'
-        title='News'
-        tag='Tag'
-        titleColor='black'
-        animationProps={{ variants: item }}
-        className='col-span-3'
-      />
-      <NewsCard
-        link='/'
-        title='News'
-        titleColor='black'
-        tag='Tag'
-        animationProps={{ variants: item }}
-        className='col-span-3'
-      />
-      <NewsCard
-        link='/'
-        title='News'
-        titleColor='black'
-        tag='Tag'
-        animationProps={{ variants: item }}
-        className='col-span-3'
-      />
-    </motion.div>
+    </>
   )
 }
